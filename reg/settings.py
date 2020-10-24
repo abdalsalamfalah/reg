f5a61387e523b6812a30876b6ae9c406ddcb2a66
abdalsalamfalah <37448148+abdalsalamfalah@users.noreply.github.com>
2020-10-24 03:53:24 +0300

    Update settings.py

--- reg/settings.py
+++ reg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%upanitux0@ct-@@kbr+^%56y6r6mr%%@*ngoe5-svg5_xx3@u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','drbaron.herokuapp.com']
 
 
 # Application definition
